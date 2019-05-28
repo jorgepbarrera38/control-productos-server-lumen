3240f2d48ae88b456f23e966d3ab411a29e2b1f4
Jorge Alexis Paz Barrera <jorgepbarrera38@gmail.com>
2019-05-28 10:52:27 -0500

    Envolviendo todas las rutas en un grupo con prefijo api

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,30 @@
 |
 */
 
-$router->group(['prefix' => 'products', 'middleware' => 'auth'], function () use ($router) {
-    $router->get('/', 'ProductController@index');
-    $router->get('/{product}', 'ProductController@show');
-    $router->post('/', 'ProductController@store');
-    $router->put('/{product}', 'ProductController@update');
-    $router->delete('/{product}', 'ProductController@destroy');
+$router->group(['prefix' => 'api'], function () use ($router) {
+
+    //Productos
+
+    $router->group(['prefix' => 'products', 'middleware' => 'auth'], function () use ($router) {
+        $router->get('/', 'ProductController@index');
+        $router->get('/{product}', 'ProductController@show');
+        $router->post('/', 'ProductController@store');
+        $router->put('/{product}', 'ProductController@update');
+        $router->delete('/{product}', 'ProductController@destroy');
+    });
+
+    //Login
+    
+    $router->group([
+        'prefix' => 'auth'
+    ], function ($router) {
+    
+        $router->post('login', 'AuthController@login');
+        $router->post('logout', 'AuthController@logout');
+        $router->post('refresh', 'AuthController@refresh');
+        $router->post('me', 'AuthController@me');
+    
+    });
+
 });
 
-//Login
-
-$router->group([
-    'prefix' => 'auth'
-], function ($router) {
-
-    $router->post('login', 'AuthController@login');
-    $router->post('logout', 'AuthController@logout');
-    $router->post('refresh', 'AuthController@refresh');
-    $router->post('me', 'AuthController@me');
-
-});
